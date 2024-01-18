f85a46249cc70171af309eb45c20d0aae866056c
Tomas Vicar <tomasvicar@gmail.com>
2024-01-18 11:48:10 +0100

    example completed

--- test_jax.py
+++ test_jax.py
@@ -14,17 +14,46 @@ import numpy as np
 import timeit
 
 
-@jit
-def add_one(img):
-            
-    return img + 1
+
+def add_one0(img):
+    for i in range(100):
+        img2 = (img * img) / img
+    return img2
             
 
 
-img = jnp.array(np.random.rand(10, 10))
+img = np.random.rand(1000, 1000)
+add_one = add_one0
+add_one(img)
+execution_time = timeit.timeit(lambda: add_one(img), number=10)
+print(execution_time)
+
+
+add_one(img)
+add_one = jit(add_one0, device=jax.devices('cpu')[0])
+img = jnp.array(np.random.rand(1000, 1000))
+
 
 execution_time = timeit.timeit(lambda: add_one(img), number=10)
 print(execution_time)
+
+
+
+
+img = np.random.rand(1000, 1000)
+add_one(img)
+add_one = jit(add_one0, device=jax.devices('gpu')[0])
+execution_time = timeit.timeit(lambda: add_one(img), number=10)
+print(execution_time)
+
+
+
+
+
+
+
+
+
 
 
 
